da4ad1b3a7eafa7ce60121a9d21079c8deb5860f
abdurrahim220 <z.abdurrahim5@gmail.com>
2023-12-24 20:37:27 +0600

    register and auth created

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,18 @@ import cors from "cors";
 import mongoose from "mongoose";
 
 import "dotenv/config";
-
+import authRoutes from './routes/auth'
+import userRoutes from './routes/users'
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+
+app.use("/api/auth",authRoutes)
+app.use("/api/users",userRoutes)
 
 app.get("/api/text", async (req: Request, res: Response) => {
   res.json("Hello from endpoint");
